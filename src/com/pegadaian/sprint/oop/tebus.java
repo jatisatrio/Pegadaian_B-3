68f359e2bb450d82cabd5422c85f61fe629ec7dc
Prams <pgustiarum@gmail.com>
2019-11-05 21:21:13 +0700

    tambahan di kelas tebus
    tambahan di kelas tebus

---
+++ src/com/pegadaian/sprint/oop/tebus.java
@@ -0,0 +1,74 @@
+package com.pegadaian.sprint.oop;
+
+public class tebus {
+
+		int idPelanggan;
+		String produk;
+		double harga;
+		String status;
+		double utang;
+
+		public tebus (){
+			
+		}
+		public tebus(int idPelanggan, String produk, double harga, String status, double utang) {
+			super();
+			this.idPelanggan = idPelanggan;
+			this.produk = produk;
+			this.harga = harga;
+			this.status = status;
+			this.utang = utang;
+		}
+
+		public int getIdPelanggan() {
+			return idPelanggan;
+		}
+
+		public void setIdPelanggan(int idPelanggan) {
+			this.idPelanggan = idPelanggan;
+		}
+
+		public String getProduk() {
+			return produk;
+		}
+
+		public void setProduk(String produk) {
+			this.produk = produk;
+		}
+
+		public double getHarga() {
+			return harga;
+		}
+
+		public void setHarga(double harga) {
+			this.harga = harga;
+		}
+
+		public String getStatus() {
+			return status;
+		}
+
+		public void setStatus(String status) {
+			this.status = status;
+		}
+
+		public double getUtang() {
+			return utang;
+		}
+
+		public void setUtang(double utang) {
+			this.utang = utang;
+		}
+
+		public double hutangLunas(double angsuran) {
+			double sisaHutang = this.utang - angsuran;
+			return sisaHutang;
+		}
+		
+		public void tampilanDataTebus (){
+			System.out.println("__________________________________________");
+			System.out.println("|" + " ID " + "|" + " Product " + "|" + " Price " + "|" + " Status " + "|"
+					+ " Loan " + "|");
+			System.out.println("__________________________________________");
+		}
+}
