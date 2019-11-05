04845c34d3c6f2c22f8076cbb0dbcfdca8f6ada4
diovanybryan <diovanybryan@gmail.com>
2019-11-05 16:27:39 +0700

    var set get inventory
    var settergetter nya inventori

--- src/com/pegadaian/sprint/oop/inventory.java
+++ src/com/pegadaian/sprint/oop/inventory.java
@@ -1,5 +1,39 @@
 package com.pegadaian.sprint.oop;
 
 public class inventory {
+	int IdPelanggan;
+	String produk;
+	String deskripsi;
+	Double harga;
+	public int getIdPelanggan() {
+		return IdPelanggan;
+	}
+	public void setIdPelanggan(int idPelanggan) {
+		IdPelanggan = idPelanggan;
+	}
+	public String getNama() {
+		return nama;
+	}
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+	public String getProduk() {
+		return produk;
+	}
+	public void setProduk(String produk) {
+		this.produk = produk;
+	}
+	public String getDeskripsi() {
+		return deskripsi;
+	}
+	public void setDeskripsi(String deskripsi) {
+		this.deskripsi = deskripsi;
+	}
+	public Double getHarga() {
+		return harga;
+	}
+	public void setHarga(Double harga) {
+		this.harga = harga;
+	}
 
 }
