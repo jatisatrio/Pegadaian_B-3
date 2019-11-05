5baaaa31031818575385a54518d6e9a2793be9f0
jojorjennifer86 <jojorjennifer@gmail.com>
2019-11-06 00:43:24 +0700

    fungsi inventory
    menambahkan fungsi inventory

--- src/com/pegadaian/sprint/oop/gadai.java
+++ src/com/pegadaian/sprint/oop/gadai.java
@@ -1,25 +1,38 @@
 package com.pegadaian.sprint.oop;
 
-public class gadai {
-String nama;
-String produkbarang;
-String deskripsi;
-double harga;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-public gadai(String nama, String produkbarang, String deskripsi, double harga, int idPelanggan) {
-	super();
-	this.nama = nama;
-	this.produkbarang = produkbarang;
-	this.deskripsi = deskripsi;
-	this.harga = harga;
-	IdPelanggan = idPelanggan;
-}
+
+public class gadai {
+	String nama;
+	String produkbarang;
+	String deskripsi;
+	String status;
+	double harga;
+	double hutang;
+	int Id;
+
+	public static int idPelanggan = 0;
+		public static String catEmas = "emas";
+		public static String catLaptop = "laptop";
+		public static String catMotor = "motor";
+		public static ArrayList<gadai> arrayListData = new ArrayList<gadai>();
+
 public String getNama() {
 	return nama;
 }
 public void setNama(String nama) {
 	this.nama = nama;
 }
+
+public String getStatus() {
+	return status;
+}
+public void setStatus(String status) {
+	this.status = status;
+}
+
 public String getProdukbarang() {
 	return produkbarang;
 }
@@ -38,8 +51,103 @@ public double getHarga() {
 public void setHarga(double harga) {
 	this.harga = harga;
 }
-public void getIdPelanggan(int idPelanggan) {
-	IdPelanggan = idPelanggan;
+public double getHutang() {
+	return hutang;
 }
-int IdPelanggan;
+public void setHutang(double hutang) {
+	this.hutang = hutang;
+}
+public int getIdPelanggan() {
+	return Id;
+}
+public void setIdPelanggan(int idPelanggan) {
+	this.Id = idPelanggan;
+}
+
+public static void MenuGadai() {
+	
+	Scanner in = new Scanner(System.in);
+	gadai dataGadai = new gadai();	
+	
+	System.out.println("Detail Information\t");
+	System.out.println("=========================");
+	System.out.print("Name\t\t\t: ");
+	
+	dataGadai.Id = idPelanggan;
+	idPelanggan++;
+	
+	boolean bName = false;
+	String nama;
+	while (!bName)
+	{
+		nama  = in.nextLine();
+		if (nama.length() < 3)
+			System.out.println("nama tidak boleh kurang dari 3 karakter");
+		else if (nama.length() > 15)
+			System.out.println("nama tidak boleh lebih dari 15 karakter");
+		else
+		{
+			bName = true;
+			dataGadai.nama = nama;
+		}
+			
+	}
+		
+	System.out.print("Product Category\t: ");
+	
+	boolean bCategory = false;
+	String category;
+	while (!bCategory)
+	{
+		category = in.nextLine();
+		if (category.trim().toUpperCase().equals(catEmas.toUpperCase()) ||  category.trim().toUpperCase().equals(catLaptop.toUpperCase())  || category.trim().toUpperCase().equals(catMotor.toUpperCase()))
+			{
+			bCategory = true;
+			dataGadai.produkbarang = category;
+			}
+		else
+			System.out.println("kategori bukan emas, laptop atau motor");
+	}
+	
+	System.out.print("Description\t\t: ");
+	
+	boolean bDescription = false;
+	String description;
+	while (!bDescription)
+	{
+		description = in.nextLine();
+		if (description.contains(" "))
+			{
+			bDescription = true;
+			dataGadai.deskripsi = description;
+			}
+		else
+			System.out.println("deskripsi kurang dari 2 kata!!");
+	}
+	
+	
+	System.out.print("Price\t\t\t: ");
+	
+	boolean bPrice = false;
+	double price;
+	while (!bPrice)
+	{
+		price = in.nextDouble();
+		if (price%10000 == 0)
+		{
+			bPrice = true;
+			dataGadai.harga = price;
+			dataGadai.hutang = price;
+		}
+		else
+			System.out.println("harga tidak kelipatan 10000");
+	}
+	
+	
+			
+	dataGadai.status = "adai";
+	
+	arrayListData.add(dataGadai);
+		System.out.println(dataGadai.harga);
+}
 }
--- src/com/pegadaian/sprint/oop/inventory.java
+++ src/com/pegadaian/sprint/oop/inventory.java
@@ -1,55 +1,32 @@
 package com.pegadaian.sprint.oop;
 
+
 public class inventory {
 	int IdPelanggan;
 	String nama;
 	String produk;
-	String deskripsi;
-	Double harga;
-
-//formatting
-	static void namaKolom() {
-		String formatter = "| %-15s | %-6s | %-10s | %-5s |%n";
-        System.out.println("-------------------------------------------------");
-        System.out.format(formatter,"ID", "Nama", "Product", "Description","Price");
-        System.out.println("-------------------------------------------------");
-    }
-	
-    void dataTable() {
-        String formatter = "| %-15s | %-5s | %-10s | %-5d |%n";
-        System.out.format(formatter, IdPelanggan, nama, produk, deskripsi, harga);
-    }
-	
-//setter getter
+	String Deskripsi;
+	Double Harga;
 	public int getIdPelanggan() {
 		return IdPelanggan;
 	}
 	public void setIdPelanggan(int idPelanggan) {
 		IdPelanggan = idPelanggan;
 	}
-	public String getNama() {
-		return nama;
+	public static void inventory(){
+		int jumlahbarang = 0; 
+		for (gadai data : gadai.arrayListData) {
+			if (data.getStatus().trim().equals("Gadai")) {
+				jumlahbarang+=1;
+		        String leftAlignFormat = "| %-15s | %-5s | %-9s | %-5d |%n";
+				System.out.println(data.getIdPelanggan()+ " "+data.getNama()+" "+data.getProdukbarang()+" "+data.getDeskripsi()+" "+data.getHarga());
+		        //System.out.format(leftAlignFormat, data.getNama() + data.getProdukbarang() + data.getDeskripsi() + data.getHarga());
+			}
+		}
+		if (jumlahbarang==0) {
+			System.out.println("Tidak ada barang gadai di inventori");
+		}
 	}
-	public void setNama(String nama) {
-		this.nama = nama;
-	}
-	public String getProduk() {
-		return produk;
-	}
-	public void setProduk(String produk) {
-		this.produk = produk;
-	}
-	public String getDeskripsi() {
-		return deskripsi;
-	}
-	public void setDeskripsi(String deskripsi) {
-		this.deskripsi = deskripsi;
-	}
-	public Double getHarga() {
-		return harga;
-	}
-	public void setHarga(Double harga) {
-		this.harga = harga;
-	}
-
+	
+	
 }
